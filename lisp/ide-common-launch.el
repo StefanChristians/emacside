f54250a4304974137c5a752299e546439ee81975
Stefan Christians <71266576+StefanChristians@users.noreply.github.com>
2025-11-01 16:13:05 +0100

    fix: standardize variable and function names

--- lisp/ide-common-launch.el
+++ lisp/ide-common-launch.el
@@ -18,52 +18,55 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; persistent cache
 
-;; last launched executable cache
-(defvar ide-common-launch-last-executable '()
-  "Alist mapping PROJECT-ROOT → last launched EXECUTABLE.")
+;; current executable cache
+(defvar ide-common-launch-current nil
+  "Mapping of currently launched executable to project.
 
-(defvar ide-common-launch-last-executable-file
-  (f-join user-emacs-directory ".cache" "ide-common-launch-last-executable.eld")
-  "Path to file storing last debugged executable per project.")
+Alist of (PROJECT . EXECUTABLE) pairs.
+
+\((project1 . executable1) (project2 . executable2))")
+
+(defvar ide-common-launch-current-file
+  (f-join user-emacs-directory ".cache" "ide-common-launch.eld")
+  "Path to file used for current executable cache.")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; cache maintenance
 
-(defun ide-common-launch-load-cache ()
+(defun ide-common-launch-load-all ()
   "Load all launch caches from disk."
-  (setq ide-common-launch-last-executable
-        (or (ide-common-read-file ide-common-launch-last-executable-file) '())))
+  (setq ide-common-launch-current
+        (or (ide-common-read-file ide-common-launch-current-file) nil)))
 
-(defun ide-common-launch-save-last-executable-cache ()
-  "Save last debugged executable cache to disk."
-  (ide-common-write-file ide-common-launch-last-executable-file
-                         ide-common-launch-last-executable))
+(defun ide-common-launch-save-current ()
+  "Save last launched executable cache to disk."
+  (ide-common-write-file ide-common-launch-current-file
+                         ide-common-launch-current))
 
-(defun ide-common-debug-save-cache ()
+(defun ide-common-launch-save-all ()
   "Save all launch caches to disk."
-  (ide-common-launch-save-last-executable-cache))
+  (ide-common-launch-save-current))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; last executable getters and setters
 
-(defun ide-common-launch-get-last-executable (project-root)
-  "Return last launched executable for PROJECT-ROOT, or nil if none."
-  (cdr (assoc project-root ide-common-launch-last-executable)))
+(defun ide-common-launch-get-current (project-root)
+  "Return last used executable for PROJECT-ROOT."
+ (alist-get project-root ide-common-launch-current nil nil #'string=))
 
-(defun ide-common-launch-set-last-executable (project-root executable)
-  "Set last launched EXECUTABLE for PROJECT-ROOT."
-  (setq ide-common-launch-last-executable
-        (cons (cons project-root executable)
-              (assoc-delete-all project-root ide-common-launch-last-executable)))
-  (ide-common-launch-save-last-executable-cache))
+(defun ide-common-launch-set-current (project-root executable)
+  "Set last used EXECUTABLE for PROJECT-ROOT."
+  (setf (alist-get project-root ide-common-launch-current nil nil #'string=)
+        executable)
+  (ide-common-launch-save-current))
 
-(defun ide-common-launch-unset-last-executable (project-root)
-  "Remove cached last launched executable for PROJECT-ROOT."
-  (setq ide-common-launch-last-executable
-        (assoc-delete-all project-root ide-common-launch-last-executable))
-  (ide-common-launch-save-last-executable-cache))
+(defun ide-common-launch-unset-current (project-root)
+  "Remove cached executable for PROJECT-ROOT."
+  (setq ide-common-launch-current
+        (assq-delete-all project-root ide-common-launch-current))
+  (ide-common-launch-save-current))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -72,19 +75,19 @@
 (defun ide-common-launch-select-executable (&optional project-root)
   "Prompt the user to select a file to launch, starting from PROJECT-ROOT.
 Caches the selection as the last launched executable for this project."
-  (ide-common-launch-load-cache)
+  (ide-common-launch-load-all)
   (let* ((root (or project-root
                      (ide-common-get-current-context-project-root)))
-         (default-file (ide-common-launch-get-last-executable root))
+         (default-file (ide-common-launch-get-current root))
          (prompt (if default-file
                      (format "Select executable (default %s): " default-file)
                    "Select executable: "))
-         (file (expand-file-name (read-file-name prompt root nil t nil) root))
+         (file (expand-file-name (read-file-name prompt root default-file t nil) root))
          (relative-file (when file
                           (ide-common-relative-path-if-descendant file root))))
     ;; Cache the last selected executable
     (when relative-file
-      (ide-common-launch-set-last-executable root relative-file))
+      (ide-common-launch-set-current root relative-file))
     relative-file))
 
 (defun ide-common-launch-execute (&optional prefix project-root)
@@ -97,20 +100,18 @@ Otherwise reuse last chosen configuration."
                   (ide-common-get-current-context-project-root)))
          (executable (or (and prefix
                               (ide-common-launch-select-executable root))
-                         (ide-common-launch-get-last-executable root)
+                         (ide-common-launch-get-current root)
                          (ide-common-launch-select-executable root)))
          ;; if executable is relative, append to root
          ;; if executable is absolute, use as is
          (program (f-join root executable))
-         (env (progn
-                (when prefix (ide-common-env-select-and-edit root))
-                (ide-common-env-load-as-list root)))
+         (env (ide-common-env-load-as-list root))
          (args (progn
                  (when prefix (ide-common-args-select-and-edit executable root))
                  (ide-common-args-get-profile
                   root
                   executable
-                  (ide-common-args-get-last-profile root executable))))
+                  (ide-common-args-get-current-profile root executable))))
          (process-environment (append env process-environment))
          (buffer-name (format "*%s*" (f-filename executable))))
     (when (comint-check-proc buffer-name)
@@ -128,7 +129,7 @@ Otherwise reuse last chosen configuration."
 ;;;; bootstrap
 
 ;; load persistent cache
-(ide-common-launch-load-cache)
+(ide-common-launch-load-all)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
